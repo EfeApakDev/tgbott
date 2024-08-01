1ca793c2d98f61c1659476f4a26c575793123103
EfeApakDev <93475651+EfeApakDev@users.noreply.github.com>
2024-08-01 07:57:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,25 @@
+from flask import Flask, request, jsonify
+import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-import os
 
-# Bot token'ınızı çevre değişkenlerinden alır
+app = Flask(__name__)
+
 TOKEN = os.getenv('7267318470:AAG0PgGQTVqHwdjLWdO4vieguGIoPdXxr_I')
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    update = Update.de_json(data, updater.bot)
+    dispatcher.process_update(update)
+    return jsonify(success=True)
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Bot çalışıyor!')
 
-def main():
-    # Updater ve Dispatcher
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    # Komut işleyiciler
-    dp.add_handler(CommandHandler("start", start))
-
-    # Botu başlat
-    updater.start_polling()
-    updater.idle()
+dispatcher.add_handler(CommandHandler('start', start))
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=5000)
